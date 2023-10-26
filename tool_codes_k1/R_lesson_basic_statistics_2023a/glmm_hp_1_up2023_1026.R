3a9a29b53333bceb434f52c483702012a62da7cf
2013zyf220 <2013zyf220@gmail.com>
2023-10-26 22:42:58 +0800

    gy_2304_231026_4

---
+++ tool_codes_k1/R_lesson_basic_statistics_2023a/glmm_hp_1_up2023_1026.R
@@ -0,0 +1,18 @@
+#refer to: Lai, Jiangshan, et al. 'glmm. hp: an R package for computing individualeffect of predictors in generalized linear mixed models.' Journal of Plant Ecology 15.6 (2022): 1302-1307.
+library(PerformanceAnalytics)
+library(car)
+library(glmm.hp)
+
+#input data
+setwd('E:/zyf_gy/zyf_gy_2304_work/tool_codes_k1/R_lesson_youtube_2023a')
+data_1 <- read.csv('housing_data.csv')
+data_2 = data_1[,-1]
+
+#build multiple linear model
+mod1 <- lm(log(price) ~ sqft.living + bedrooms + bathrooms, data = data_2)
+mod1
+
+#glmm.hp analysis
+glmm_hp_mod <- glmm.hp(mod1)
+glmm_hp_mod
+plot(glmm.hp(mod1))
