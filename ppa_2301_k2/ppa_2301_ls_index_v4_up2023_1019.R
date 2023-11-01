99e9f6784fe018c113b3f65c3de46d3b2809cfa5
2013zyf220 <2013zyf220@gmail.com>
2023-11-01 12:00:13 +0800

    gy_2304_231101_2

--- ppa_2301_k2/ppa_2301_ls_index_v4_up2023_1019.R
+++ ppa_2301_k2/ppa_2301_ls_index_v4_up2023_1019.R
@@ -7,7 +7,7 @@ library(tidyr)
 library(landscapemetrics)
 library(ggplot2)
 #rm(list = ls())
-#list_lsm()
+#list_lsm() #show lsm indexes that can be calculated in 'landscapemetrics'
 
 #============================================================================
 
@@ -60,10 +60,10 @@ ls_index_f <- function(f_year){
     fc_grid_x <- f_grid_1b@polygons[[ii]]@labpt[1];
     fc_grid_y <- f_grid_1b@polygons[[ii]]@labpt[2];
     
-    fc_lss_0 <- sample_lsm(f_luse, f_grid_2[ii,], what = c('lsm_c_pland','lsm_c_cohesion', 'lsm_c_ai'), shape = 'square'); #to_be_set
+    fc_lss_0 <- sample_lsm(f_luse, f_grid_2[ii,], what = c('lsm_c_pland','lsm_c_cohesion', 'lsm_c_ai','lsm_l_pd','lsm_l_lsi'), shape = 'square'); #to_be_set
     fc_lsc_0 <- sample_lsm(f_luse, f_grid_2[ii,], what = c('lsm_c_pland'), shape = 'circle'); #to_be_set
     
-    fc_class_lss <- data.frame(class = rep(c(1:8),5), metric=c(rep('pland',8),rep('cohesion',8), rep('ai',8), rep('pd',8), rep('lsi',8))) #to_be_set
+    fc_class_lss <- data.frame(class = rep(c(1:8),3), metric=c(rep('pland',8),rep('cohesion',8), rep('ai',8))) #to_be_set
     fc_lss <- left_join(fc_class_lss, fc_lss_0)
     fc_lss$value <- ifelse(is.na(fc_lss$value), 0, fc_lss$value)
     
@@ -89,18 +89,14 @@ ls_index_f <- function(f_year){
     fc_ai_wat <- fc_ai$value[fc_ai$class == 5]
     fc_ai_imp <- fc_ai$value[fc_ai$class == 8]
     
-    fc_pd <- fc_lss %>% filter(metric == 'pd') #patch density
+    fc_pd <- fc_lss_0 %>% filter(metric == 'pd') #patch density
     fc_pd$value <- as.numeric(round(fc_pd$value,2))
-    fc_pd_gre <- fc_pd$value[fc_pd$class == 1]
-    fc_pd_wat <- fc_pd$value[fc_pd$class == 5]
-    fc_pd_imp <- fc_pd$value[fc_pd$class == 8]
+    fc_pd_v <- fc_pd$value
     
-    fc_lsi <- fc_lss %>% filter(metric == 'lsi') #patch density
+    fc_lsi <- fc_lss_0 %>% filter(metric == 'lsi') #patch density
     fc_lsi$value <- as.numeric(round(fc_lsi$value,2))
-    fc_lsi_gre <- fc_lsi$value[fc_lsi$class == 1]
-    fc_lsi_wat <- fc_lsi$value[fc_lsi$class == 5]
-    fc_lsi_imp <- fc_lsi$value[fc_lsi$class == 8]
-    
+    fc_lsi_v <- fc_lsi$value
+
     fc_pland_c <- fc_lsc %>% filter(metric == 'pland')
     fc_pland_c$value <- as.numeric(round(fc_pland_c$value,2))
     fc_pland_c_gre <- fc_pland_c$value[fc_pland_c$class == 1]
@@ -109,11 +105,11 @@ ls_index_f <- function(f_year){
     
     fc_df <- data.frame(rx_grid_id = fc_grid_id, rx_grid_x = fc_grid_x, rx_grid_y = fc_grid_y, 
                        rx_pland_s_imp = fc_pland_s_imp, rx_cohesion_imp = fc_cohesion_imp, rx_pland_c_imp = fc_pland_c_imp, 
-                       rx_ai_imp = fc_ai_imp, rx_pd_imp = fc_pd_imp,  rx_lsi_imp = fc_lsi_imp,
+                       rx_ai_imp = fc_ai_imp, 
                        rx_pland_s_gre = fc_pland_s_gre, rx_cohesion_gre = fc_cohesion_gre, rx_pland_c_gre = fc_pland_c_gre, 
-                       rx_ai_gre = fc_ai_gre, rx_pd_gre = fc_pd_gre,  rx_lsi_gre = fc_lsi_gre,
+                       rx_ai_gre = fc_ai_gre, 
                        rx_pland_s_wat = fc_pland_s_wat, rx_cohesion_wat = fc_cohesion_wat, rx_pland_c_wat = fc_pland_c_wat, 
-                       rx_ai_wat = fc_ai_wat, rx_pd_wat = fc_pd_wat,  rx_lsi_wat = fc_lsi_wat) 
+                       rx_ai_wat = fc_ai_wat, rx_pd = fc_pd_v,  rx_lsi = fc_lsi_v) 
     
     f_ls_metric <- rbind(f_ls_metric,fc_df)
   
@@ -124,20 +120,16 @@ ls_index_f <- function(f_year){
     f_grid_2$rx_pc_imp[ii] <- fc_pland_c_imp
     f_grid_2$rx_co_imp[ii] <- fc_cohesion_imp
     f_grid_2$rx_ai_imp[ii] <- fc_ai_imp
-    f_grid_2$rx_pd_imp[ii] <- fc_pd_imp
-    f_grid_2$rx_lsi_imp[ii] <- fc_lsi_imp
     f_grid_2$rx_ps_gre[ii] <- fc_pland_s_gre
     f_grid_2$rx_pc_gre[ii] <- fc_pland_c_gre
     f_grid_2$rx_co_gre[ii] <- fc_cohesion_gre
     f_grid_2$rx_ai_gre[ii] <- fc_ai_gre
-    f_grid_2$rx_pd_gre[ii] <- fc_pd_gre
-    f_grid_2$rx_lsi_gre[ii] <- fc_lsi_gre
     f_grid_2$rx_ps_wat[ii] <- fc_pland_s_wat
     f_grid_2$rx_pc_wat[ii] <- fc_pland_c_wat
     f_grid_2$rx_co_wat[ii] <- fc_cohesion_wat
     f_grid_2$rx_ai_wat[ii] <- fc_ai_wat
-    f_grid_2$rx_pd_wat[ii] <- fc_pd_wat
-    f_grid_2$rx_lsi_wat[ii] <- fc_lsi_wat
+    f_grid_2$rx_pd[ii] <- fc_pd_v
+    f_grid_2$rx_lsi[ii] <- fc_lsi_v
   }
   
   st_write(f_grid_2, paste0('2301_river_4_', f_year,'.shp'))
@@ -147,8 +139,8 @@ ls_index_f <- function(f_year){
 
 #=========================================================================
 
-year_s <- 2022; #to_be_set
-year_e <- 2022; #to_be_set
+year_s <- 2021; #to_be_set
+year_e <- 2021; #to_be_set
 
 for(c_year in year_s: year_e){
   ls_index_f(c_year)
