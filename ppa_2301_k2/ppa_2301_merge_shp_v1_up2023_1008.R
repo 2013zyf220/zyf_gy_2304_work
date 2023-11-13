45dcd02705e3c2cd2c3cbb35441bcb2d3db403ed
2013zyf220 <2013zyf220@gmail.com>
2023-11-13 17:53:14 +0800

    gy_2304_231113_2

--- ppa_2301_k2/ppa_2301_merge_shp_v1_up2023_1008.R
+++ ppa_2301_k2/ppa_2301_merge_shp_v1_up2023_1008.R
@@ -1,6 +1,6 @@
 library(sf)
 
-merge_res <- function(f_year){
+merge_f <- function(f_year){
   setwd('E:/zyf_gn/zyf_gn_2301_data/ppa_2301_k2/shp/outputs2');
   f_shp_1a <- st_read(paste0('2301_river_5b_', f_year,'.shp'))
   
@@ -13,12 +13,29 @@ merge_res <- function(f_year){
   
   f_merged_shp <- merge(f_shp_1b, f_shp_2b, by = 'NUMBER')
   write.csv(f_merged_shp, paste0('2301_river_6_',f_year,'.csv'), row.names = FALSE)
+  
+  f_shp_1a$XG_ANGLE_1 <- f_shp_2a$XG_ANGLE_1
+  f_shp_1a$XG_ANGLE_2 <- f_shp_2a$XG_ANGLE_2
+  f_shp_1a$NUMBER2 <- f_shp_2a$NUMBER
+  #st_write(f_shp_1a, paste0('2301_river_6_', c_year,'.shp'))
+  
+  res_list <- list();
+  res_list[['shp_1a']] <- f_shp_1a
+  res_list[['shp_2a']] <- f_shp_2a
+  res_list[['shp_1b']] <- f_shp_1b
+  res_list[['shp_2b']] <- f_shp_2b
+  res_list[['merged_shp']] <- f_merged_shp
+  return(res_list)
 }
 
 #=====================================
 
 year_s <- 2021; #to_be_set
 year_e <- 2021; #to_be_set
+
+merge_res <- list();
+ii <- 1;
 for(c_year in year_s: year_e){
-  merge_res(c_year)
+  merge_res[[ii]] <- merge_f(c_year)
+  ii <- ii + 1
 }
