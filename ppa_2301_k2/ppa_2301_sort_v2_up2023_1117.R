510a2ae8eaffd323e1edae23538d725179a120af
2013zyf220 <2013zyf220@gmail.com>
2023-11-22 15:49:20 +0800

    gy_2304_231122_6

--- ppa_2301_k2/ppa_2301_sort_v2_up2023_1117.R
+++
@@ -1,6 +0,0 @@
-library(sf)
-setwd('E:/zyf_gn/zyf_gn_2301_data/ppa_2301_k2/shp/1')
-grid_a1 <- st_read('2301_cq_water_b10_buf500_a19.shp')
-grid_b1 <- grid_a1[order(grid_a1$NUMBER), ]
-
-st_write(grid_b1, '2301_cq_water_b10_buf500_a20.shp')
---
+++ ppa_2301_k2/ppa_2301_sort_v2_up2023_1122.R
@@ -0,0 +1,32 @@
+library(sf)
+setwd('E:/zyf_gn/zyf_gn_2301_data/ppa_2301_k2/shp')
+
+grid_paths_1 <- list();
+grid_paths_1[[1]] <- '1/2301_cq_water_b10_buf500_a19.shp';
+grid_paths_1[[2]] <- '2/2301_cq_water_b11_buf200_a05.shp';
+grid_paths_1[[3]] <- '2/2301_cq_water_b11_buf400_a04.shp';
+grid_paths_1[[4]] <- '2/2301_cq_water_b11_buf500_a01.shp';
+grid_paths_1[[5]] <- '2/2301_cq_water_b11_buf600_a08.shp';
+grid_paths_1[[6]] <- '2/2301_cq_water_b11_buf800_a09.shp';
+grid_paths_1[[7]] <- '2/2301_cq_water_b11_buf1000_a16.shp';
+
+grid_paths_2 <- list();
+grid_paths_2[[1]] <- '1/2301_cq_water_b12_buft500_a01.shp';
+grid_paths_2[[2]] <- '2/2301_cq_water_b12_buf200_a01.shp';
+grid_paths_2[[3]] <- '2/2301_cq_water_b12_buf400_a01.shp';
+grid_paths_2[[4]] <- '2/2301_cq_water_b12_buf500_a01.shp';
+grid_paths_2[[5]] <- '2/2301_cq_water_b12_buf600_a01.shp';
+grid_paths_2[[6]] <- '2/2301_cq_water_b12_buf800_a01.shp';
+grid_paths_2[[7]] <- '2/2301_cq_water_b12_buf1000_a01.shp';
+
+sort_f <- function(f_path){
+  f_grid_1 <- st_read(f_path);
+  f_grid_2 <- f_grid_1[order(f_grid_1$NUMBER), ]
+  return(f_grid_2)
+}
+
+grid_res <- list();
+for(ii in 1:7){
+  grid_res[[ii]] <- sort_f(grid_paths_1[[ii]])
+  st_write(grid_res[[ii]], grid_paths_2[[ii]])
+}
