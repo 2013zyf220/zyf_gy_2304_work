da413ff1be3d2bb1535fd33f679a0e3ae8b7e3ff
2013zyf220 <2013zyf220@gmail.com>
2023-11-29 08:25:12 +0800

    gy_2304_231129_3

--- ppa_2301_k2/ppa_2301_ls_index_v6_up2023_1129.R
+++ ppa_2301_k2/ppa_2301_ls_index_v6_up2023_1129.R
@@ -33,20 +33,24 @@ ls_index_f <- function(f_year, f_buffer){
   f_grid_2$XL_co_imp <- rep(0, f_len_grid_2)
   f_grid_2$XL_ai_imp <- rep(0, f_len_grid_2)
   f_grid_2$XL_shape_mn_imp <- rep(0, f_len_grid_2)
+  f_grid_2$XL_enn_mn_imp <- rep(0, f_len_grid_2)
   f_grid_2$XL_ps_gre <- rep(0, f_len_grid_2)
   f_grid_2$XL_pc_gre <- rep(0, f_len_grid_2)
   f_grid_2$XL_co_gre <- rep(0, f_len_grid_2)
   f_grid_2$XL_ai_gre <- rep(0, f_len_grid_2)
   f_grid_2$XL_shape_mn_gre <- rep(0, f_len_grid_2)
+  f_grid_2$XL_enn_mn_gre <- rep(0, f_len_grid_2)
   f_grid_2$XL_ps_wat <- rep(0, f_len_grid_2)
   f_grid_2$XL_pc_wat <- rep(0, f_len_grid_2)
   f_grid_2$XL_co_wat <- rep(0, f_len_grid_2)
   f_grid_2$XL_ai_wat <- rep(0, f_len_grid_2)
   f_grid_2$XL_shape_mn_wat <- rep(0, f_len_grid_2)
+  f_grid_2$XL_enn_mn_wat <- rep(0, f_len_grid_2)
   f_grid_2$XL_pd <- rep(0, f_len_grid_2)
   f_grid_2$XL_lsi <- rep(0, f_len_grid_2)
   f_grid_2$XL_shdi <- rep(0, f_len_grid_2)
   f_grid_2$XL_frac_mn <- rep(0, f_len_grid_2)
+  f_grid_2$XL_cohesion <- rep(0, f_len_grid_2)
   f_ls_metric <- data.frame()
   
   for (ii in 1: f_len_grid_1) {
@@ -55,7 +59,7 @@ ls_index_f <- function(f_year, f_buffer){
     fc_grid_x <- f_grid_1b@polygons[[ii]]@labpt[1];
     fc_grid_y <- f_grid_1b@polygons[[ii]]@labpt[2];
     
-    fc_lss_0 <- sample_lsm(f_luse, f_grid_1b[ii,], what = c('lsm_c_pland','lsm_c_cohesion', 'lsm_c_ai','lsm_c_shape_mn','lsm_l_pd','lsm_l_lsi','lsm_l_shdi','lsm_l_frac_mn'), shape = 'square'); #to_be_set
+    fc_lss_0 <- sample_lsm(f_luse, f_grid_1b[ii,], what = c('lsm_c_pland','lsm_c_cohesion', 'lsm_c_ai','lsm_c_shape_mn','lsm_c_enn_mn','lsm_l_pd','lsm_l_lsi','lsm_l_shdi','lsm_l_frac_mn','lsm_l_cohesion'), shape = 'square'); #to_be_set
     fc_lsc_0 <- sample_lsm(f_luse, f_grid_1b[ii,], what = c('lsm_c_pland'), shape = 'circle'); #to_be_set
     
     fc_class_lss <- data.frame(class = rep(c(1:8),3), metric=c(rep('pland',8),rep('cohesion',8), rep('ai',8))) #to_be_set
@@ -89,6 +93,12 @@ ls_index_f <- function(f_year, f_buffer){
     fc_shape_mn_gre <- fc_shape_mn$value[fc_shape_mn$class == 1]
     fc_shape_mn_wat <- fc_shape_mn$value[fc_shape_mn$class == 5]
     fc_shape_mn_imp <- fc_shape_mn$value[fc_shape_mn$class == 8]
+
+    fc_enn_mn <- fc_lss %>% filter(metric == 'enn_mn') #aggregation index
+    fc_enn_mn$value <- as.numeric(round(fc_enn_mn$value,2))
+    fc_enn_mn_gre <- fc_enn_mn$value[fc_enn_mn$class == 1]
+    fc_enn_mn_wat <- fc_enn_mn$value[fc_enn_mn$class == 5]
+    fc_enn_mn_imp <- fc_enn_mn$value[fc_enn_mn$class == 8]
     
     fc_pd <- fc_lss_0 %>% filter(metric == 'pd') #patch density
     fc_pd$value <- as.numeric(round(fc_pd$value,2))
@@ -105,6 +115,10 @@ ls_index_f <- function(f_year, f_buffer){
     fc_frac_mn <- fc_lss_0 %>% filter(metric == 'frac_mn') #patch density
     fc_frac_mn$value <- as.numeric(round(fc_frac_mn$value,2))
     fc_frac_mn_v <- fc_frac_mn$value
+
+    fc_cohesion <- fc_lss_0 %>% filter(metric == 'fc_cohesion') #patch density
+    fc_cohesion$value <- as.numeric(round(fc_cohesion$value,2))
+    fc_cohesion_v <- fc_cohesion$value
     
     fc_pland_c <- fc_lsc %>% filter(metric == 'pland')
     fc_pland_c$value <- as.numeric(round(fc_pland_c$value,2))
@@ -114,12 +128,12 @@ ls_index_f <- function(f_year, f_buffer){
     
     fc_df <- data.frame(NUMBER = fc_grid_id, XL_grid_x = fc_grid_x, XL_grid_y = fc_grid_y, 
                        XL_ps_imp = fc_pland_s_imp, XL_co_imp = fc_cohesion_imp, XL_pc_imp = fc_pland_c_imp, 
-                       XL_ai_imp = fc_ai_imp, XL_shape_mn_imp = fc_shape_mn_imp,
+                       XL_ai_imp = fc_ai_imp, XL_shape_mn_imp = fc_shape_mn_imp, XL_enn_mn_imp = fc_enn_mn_imp,
                        XL_ps_gre = fc_pland_s_gre, XL_co_gre = fc_cohesion_gre, XL_pc_gre = fc_pland_c_gre, 
-                       XL_ai_gre = fc_ai_gre, XL_shape_mn_gre = fc_shape_mn_gre,
+                       XL_ai_gre = fc_ai_gre, XL_shape_mn_gre = fc_shape_mn_gre, XL_enn_mn_gre = fc_enn_mn_gre,
                        XL_ps_wat = fc_pland_s_wat, XL_co_wat = fc_cohesion_wat, XL_pc_wat = fc_pland_c_wat, 
-                       XL_ai_wat = fc_ai_wat, XL_shape_mn_wat = fc_shape_mn_wat,
-                       XL_pd = fc_pd_v, XL_lsi = fc_lsi_v, XL_shdi = fc_shdi_v, XL_frac_mn = fc_frac_mn_v) 
+                       XL_ai_wat = fc_ai_wat, XL_shape_mn_wat = fc_shape_mn_wat, XL_enn_mn_wat = fc_enn_mn_wat,
+                       XL_pd = fc_pd_v, XL_lsi = fc_lsi_v, XL_shdi = fc_shdi_v, XL_frac_mn = fc_frac_mn_v, XL_cohesion = fc_cohesion_v) 
     
     f_ls_metric <- rbind(f_ls_metric,fc_df)
   
@@ -130,21 +144,25 @@ ls_index_f <- function(f_year, f_buffer){
     f_grid_2$XL_pc_imp[ii] <- fc_pland_c_imp
     f_grid_2$XL_co_imp[ii] <- fc_cohesion_imp
     f_grid_2$XL_ai_imp[ii] <- fc_ai_imp
-    f_grid_2$XL_ai_imp[ii] <- fc_shape_mn_imp
+    f_grid_2$XL_shape_mn_imp[ii] <- fc_shape_mn_imp
+    f_grid_2$XL_enn_mn_imp[ii] <- fc_enn_mn_imp
     f_grid_2$XL_ps_gre[ii] <- fc_pland_s_gre
     f_grid_2$XL_pc_gre[ii] <- fc_pland_c_gre
     f_grid_2$XL_co_gre[ii] <- fc_cohesion_gre
     f_grid_2$XL_ai_gre[ii] <- fc_ai_gre
-    f_grid_2$XL_ai_gre[ii] <- fc_shape_mn_gre
+    f_grid_2$XL_shape_mn_gre[ii] <- fc_shape_mn_gre
+    f_grid_2$XL_enn_mn_gre[ii] <- fc_enn_mn_gre
     f_grid_2$XL_ps_wat[ii] <- fc_pland_s_wat
     f_grid_2$XL_pc_wat[ii] <- fc_pland_c_wat
     f_grid_2$XL_co_wat[ii] <- fc_cohesion_wat
     f_grid_2$XL_ai_wat[ii] <- fc_ai_wat
-    f_grid_2$XL_ai_wat[ii] <- fc_shape_mn_wat
+    f_grid_2$XL_shape_mn_wat[ii] <- fc_shape_mn_wat
+    f_grid_2$XL_enn_mn_wat[ii] <- fc_enn_mn_wat
     f_grid_2$XL_pd[ii] <- fc_pd_v
     f_grid_2$XL_lsi[ii] <- fc_lsi_v
     f_grid_2$XL_shdi[ii] <- fc_shdi_v
     f_grid_2$XL_frac_mn[ii] <- fc_frac_mn_v
+    f_grid_2$XL_cohesion[ii] <- fc_cohesion_v
   }
   
   st_write(f_grid_2, paste0('shp/3/ppa_2301_lsi_', f_year, '_buf', f_buffer, '.shp'))
