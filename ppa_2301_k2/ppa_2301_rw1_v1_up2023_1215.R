7dfaf1181a3d6f584f94071396c4e82ddfb72afb
2013zyf220 <2013zyf220@gmail.com>
2023-12-15 14:03:25 +0800

    gy_2304_231215_3

--- ppa_2301_k2/ppa_2301_rw1_v1_up2023_1215.R
+++ ppa_2301_k2/ppa_2301_rw1_v1_up2023_1215.R
@@ -12,4 +12,5 @@ setwd("E:/zyf_gn/zyf_gn_2301_data/ppa_2301_k2/shp/rw")
 rw_1 <- st_read("2301_cq_water_rw_7.shp")
 rw_2 <- as.data.frame(rw_1)
 rw_3 <- rw_2[,c("line_num","line_len")]
+colnames(rw_3) <- c("NUMBER","XA_RIVERW")
 write.csv(rw_3, "2301_cq_water_rw_7.csv")
