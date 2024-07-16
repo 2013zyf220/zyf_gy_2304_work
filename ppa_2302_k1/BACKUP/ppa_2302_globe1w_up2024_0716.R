5429db2061c41bdee9574d46bc8679b562df3e2e
2013zyf220 <2013zyf220@gmail.com>
2024-07-16 10:15:13 +0800

    gy_2304_240716_11

--- ppa_2302_k1/BACKUP/ppa_2302_globe1w_up2024_0716.R
+++ ppa_2302_k1/BACKUP/ppa_2302_globe1w_up2024_0716.R
@@ -39,56 +39,6 @@ len_sites <- 50 #to_be_set
 
 days_ori_name <- c('day1', 'day2', 'day3', 'day4', 'day5', 'day6')
 
-#============================================
-#up2024_0709_07:06
-
-cal_globe_1f <- function(f_vari, f_time, f_str, f_day){
-  f_rou <- str2rou_f(f_str)
-  f_data_1 <- read.csv(paste0('ORI_GLOBE/GLOBE_NO', f_rou, '.csv'))
-  if(f_str%%2 == 1){
-    f_s1 <- (f_day - 1) * 360 + (f_time - 1) * 120 + 1
-    f_s2 <- f_s1 + len_sites - 1
-    f_data_2 <- f_data_1[[f_vari]][f_s1: f_s2]
-  }else{
-    f_s1 <- (f_day - 1) * 360 + (f_time - 1) * 120 + 61
-    f_s2 <- f_s1 + len_sites - 1
-    f_data_2 <- rev(f_data_1[[f_vari]][f_s1: f_s2])
-  }
-  
-  return(f_data_2)
-}
-
-#============================================
-#up2024_0709_07:21
-
-cal_globe_2f <- function(f_vari){
-  f_data_1 <- list()
-  f_data_1_df <- list()
-  
-  for(ii in times_set){
-    f_data_1[[ii]] <- matrix(0, nrow = len_sites * len_strs_co, ncol = len_days_ori)
-    for(jj in strs_co){
-      f_s <- (jj - 1) * len_sites + 1
-      f_e <- jj * len_sites
-      for(kk in days_ori){
-        f_data_1[[ii]][f_s:f_e,kk] <- cal_globe_1f(f_vari, ii, jj, kk)
-      }
-    }
-    f_data_1_df[[ii]] <- as.data.frame(f_data_1[[ii]])
-    colnames(f_data_1_df[[ii]]) <- days_ori_name
-    write.csv(f_data_1_df[[ii]], paste0('RES1/datag_1_', f_vari, '_time', ii,'.csv'), row.names = FALSE)
-  }
-  return(f_data_1_df)
-}
-
-#=========================================
-#up2024_0709_07:21
-
-cal_globe_res_1 <- list()
-for(c_vari in varis_1){
-  cal_globe_res_1[[c_vari]] <- cal_globe_2f(c_vari)
-}
-
 #======================================================================
 #======================================================================
 #up2024_0709_07:21
