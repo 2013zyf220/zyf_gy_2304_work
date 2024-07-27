0351fe1fd267ea4f2a4989c648fba277baaca0fc
2013zyf220 <2013zyf220@gmail.com>
2024-07-27 16:16:54 +0800

    gy_2304_240727_2

--- ppa_2302_k1/ppa_2302_rayman_prepare_comparison.R
+++ ppa_2302_k1/ppa_2302_rayman_prepare_comparison.R
@@ -1,4 +1,4 @@
-library(MASS)
+gy_2304_240727_1library(MASS)
 library(skimr)
 library(DataExplorer)
 library(tidyverse)
@@ -11,7 +11,6 @@ setwd('E:/zyf_gn/zyf_gn_2301_data/ppa_2302_k2/ARCGIS')
 
 #==================
 #up2024_0712_22:03
-paras <- c('rec', 'recg2', 'recw2') #to_be_set_key
 
 len_sites <- 50 #to_be_set
 len_strs_co <- 8  #to_be_set
@@ -73,60 +72,35 @@ for(ii in times_set){
 }
 
 #==================
-#up2024_0724_11:00
+#up2024_0712_22:06
 
-get_tp_f <- function(f_para){
-  f_tp_1 <- list()
-  f_tp_2 <- list()
-  for(ii in times_set){
-    f_tp_1[[ii]] <- as.matrix(read.csv(paste0('RES2/', f_para, '_1_TP_time', ii, '.csv'))) #to_be_set_key
-    f_tp_2[[ii]] <- as.vector(f_tp_1[[ii]])
-  }
-  return(f_tp_2)
-}
-
-tp_data <- list()
-for(c_para in paras){
-  tp_data[[c_para]] <- get_tp_f(c_para)
-}
-
-#==================
-#up2024_0724_11:00
-
-get_rh_f <- function(f_para){
-  f_rh_1 <- list()
-  f_rh_2 <- list()
-  for(ii in times_set){
-    f_rh_1[[ii]] <- as.matrix(read.csv(paste0('RES2/', f_para, '_1_RH_time', ii, '.csv'))) #to_be_set_key
-    f_rh_2[[ii]] <- as.vector(f_rh_1[[ii]])
-  }
-  return(f_rh_2)
-}
-
-rh_data <- list()
-for(c_para in paras){
-  rh_data[[c_para]] <- get_rh_f(c_para)
+tp_1 <- list()
+tp_2 <- list()
+for(ii in times_set){
+  tp_1[[ii]] <- as.matrix(read.csv(paste0('RES2/rec_1_TP_time', ii, '.csv'))) #to_be_set_key
+  tp_2[[ii]] <- as.vector(tp_1[[ii]])
 }
 
 
 #==================
-#up2024_0724_11:00
+#up2024_0712_22:06
 
-get_ws_f <- function(f_para){
-  f_ws_1 <- list()
-  f_ws_2 <- list()
-  for(ii in times_set){
-    f_ws_1[[ii]] <- as.matrix(read.csv(paste0('RES2/', f_para, '_1_1WS_time', ii, '.csv'))) #to_be_set_key
-    f_ws_2[[ii]] <- as.vector(f_ws_1[[ii]])
-  }
-  return(f_ws_2)
+rh_1 <- list()
+rh_2 <- list()
+for(ii in times_set){
+  rh_1[[ii]] <- as.matrix(read.csv(paste0('RES2/rec_1_RH_time', ii, '.csv')))  #to_be_set_key
+  rh_2[[ii]] <- as.vector(rh_1[[ii]])
 }
 
-ws_data <- list()
-for(c_para in paras){
-  ws_data[[c_para]] <- get_ws_f(c_para)
-}
+#==================
+#up2024_0712_22:06
 
+ws_1 <- list()
+ws_2 <- list()
+for(ii in times_set){
+  ws_1[[ii]] <- as.matrix(read.csv(paste0('RES2/recw2_1_1WS_time', ii, '.csv')))  #to_be_set_key
+  ws_2[[ii]] <- round(as.vector(ws_1[[ii]]), 2)
+}
 
 #==================
 #up2024_0712_22:06
@@ -134,39 +108,28 @@ for(c_para in paras){
 could_1 <- 0.0 #to_be_set
 could_2 <- rep(could_1, len_sites * len_strs_co * len_days_ori)
 
-#===============================
-#up2024_0724_11:00
+#==================
+#up2024_0712_22:06
 
 col_names_set <- c('DATE', 'TIME', 'LON', 'LAT', 'ELE', 'TIMEZONE', 'TP', 'RH', 'WS', 'CLOUD') #to_be_set
 
-rayman_f1 <- function(f_para_tr, f_para_ws, f_time){
-  f_rayman_1 <- matrix(0, nrow = len_sites * len_strs_co * len_days_ori, ncol = 10) #to_be_set
-  f_rayman_1[,1] <- date_3
-  f_rayman_1[,2] <- time_sum[[f_time]]
-  f_rayman_1[,3] <- lon_2
-  f_rayman_1[,4] <- lat_2
-  f_rayman_1[,5] <- ele_2
-  f_rayman_1[,6] <- timez_2
-  f_rayman_1[,7] <- tp_data[[f_para_tr]][[f_time]]
-  f_rayman_1[,8] <- rh_data[[f_para_tr]][[f_time]]
-  f_rayman_1[,9] <- ws_data[[f_para_ws]][[f_time]]
-  f_rayman_1[,10] <- could_2
-  f_rayman_1_df <- as.data.frame(f_rayman_1)
-  colnames(f_rayman_1_df) <- col_names_set
-  write.csv(f_rayman_1_df, paste0('RAYMAN/rayman_1_time', ii, '_', f_para_tr, '_', f_para_ws, '.csv'), row.names = FALSE)
-  write.table(f_rayman_1_df, file = paste0('RAYMAN/rayman_1_time', ii, '_', f_para_tr, '_', f_para_ws, '.txt'), sep = ' ', row.names = FALSE, col.names = TRUE, quote = FALSE)
+rayman_1 <- list()
+rayman_1_df <- list()
+for(ii in times_set){
+  rayman_1[[ii]] <- matrix(0, nrow = len_sites * len_strs_co * len_days_ori, ncol = 10) #to_be_set
+  rayman_1[[ii]][,1] <- date_3
+  rayman_1[[ii]][,2] <- time_sum[[ii]]
+  rayman_1[[ii]][,3] <- lon_2
+  rayman_1[[ii]][,4] <- lat_2
+  rayman_1[[ii]][,5] <- ele_2
+  rayman_1[[ii]][,6] <- timez_2
+  rayman_1[[ii]][,7] <- tp_2[[ii]]
+  rayman_1[[ii]][,8] <- rh_2[[ii]]
+  rayman_1[[ii]][,9] <- ws_2[[ii]]
+  rayman_1[[ii]][,10] <- could_2
+  rayman_1_df[[ii]] <- as.data.frame(rayman_1[[ii]])
+  colnames(rayman_1_df[[ii]]) <- col_names_set
+  write.csv(rayman_1_df[[ii]], paste0('RES2/rayman_1_time', ii, '.csv'), row.names = FALSE)
+  write.table(rayman_1_df[[ii]], file = paste0('RES2/rayman_1_time', ii, '.txt'), sep = ' ', row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
 
-#===============================
-#up2024_0724_11:00
-
-rayman_res <- list()
-for(c_para_tr in paras){
-  rayman_res[[c_para_tr]] <- list()
-  for(c_para_ws in paras){
-    rayman_res[[c_para_tr]][[c_para_ws]] <- list()
-    for(ii in times_set){
-      rayman_res[[c_para_tr]][[c_para_ws]][[ii]] <- rayman_f1(c_para_tr, c_para_ws, ii)
-    }
-  }
-}
